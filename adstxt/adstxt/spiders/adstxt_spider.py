6537aa71521cf0a8104625cd1ca2d5db243e5b99
kaustubhd93 <kaustubhdesai93@gmail.com>
2020-01-12 21:10:59 +0530

    Added proper option for taking domain file name as command line arguement

--- adstxt/adstxt/spiders/adstxt_spider.py
+++ adstxt/adstxt/spiders/adstxt_spider.py
@@ -3,14 +3,11 @@ import scrapy
 sys.path.append('../')
 from adstxtcrawler import get_ads_txt
 
-domainDetails = sys.argv[1]
-
 class AdstxtSpider(scrapy.Spider):
     name = "adstxt"
 
-    def start_requests(self):
-        #fObj = open("/home/kaustubh/Workspace/adstxt-crawler/sample", "r+")
-        fObj = open(self.domainDetails, "r+")
+    def start_requests(self, fileName=""):
+        fObj = open(self.fileName, "r+")
         domainList = fObj.readlines()
         fObj.close()
         domainList = map(lambda x: x.strip("\n"), domainList)
@@ -18,11 +15,6 @@ class AdstxtSpider(scrapy.Spider):
             yield scrapy.Request(url="http://{}/ads.txt".format(domain), callback=self.parse)
 
     def parse(self, response):
-        #print dir(response)
         domain = response.url.split("/")[-2]
-        # filename = 'adstxt-%s.txt' % page
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log('Saved file %s' % filename)
         get_ads_txt(domain, response.body, True)
         self.log('Saved file *********************************************')
